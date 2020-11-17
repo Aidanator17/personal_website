6d725198550bda4cd1d727912e67fbaeb35fec4b
Aidan Christopher <gandamovies@gmail.com>
2020-11-17 15:52:37 -0800

    almost done js code

---
+++ scripts/courses.js
@@ -0,0 +1,26 @@
+courseList = {  
+                class1 : {code: 'ACIT 1620',  name: 'Fundamental Web Technologies'}, 
+                class2 : {code: 'ACIT 1515',  name: 'Scripting for IT'}, 
+                class3 : {code: 'ACIT 1420',  name: 'Introduction to Systems Administration'}
+            }
+do { 
+    var x = prompt("Input course code:");
+}
+while(
+    typeof parseInt(x) !== 'number'
+    );
+
+let create = true
+for (i in courseList) {
+    for (a in courseList[i]) {
+        if (courseList[i][a] == 'ACIT '+ x) {
+            console.log("Yes I am taking the course: "+courseList[i]['code'],"-", courseList[i]['name'])
+            create = false
+        }
+    }
+    
+    }
+if (create == true){
+    courseList['class4'] = {code: "ACIT "+x, name: null}
+    console.log("Successfully added class "+courseList['class4']['code'])
+}
